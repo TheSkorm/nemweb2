ded46fb8832195ebd6414053a7f0865499488886
Michael Wheeler <michael.wheeler@tigerspike.com>
2018-01-26 10:35:24 +1100

    Added really basic documentation

---
+++ docs/index.rst
@@ -0,0 +1,25 @@
+.. nem documentation master file, created by
+   sphinx-quickstart on Fri Jan 26 09:08:40 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to nem's documentation!
+===============================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. automodule:: nem
+    :members:
+    :show-inheritance:
+.. automodule:: nem.importer
+    :members:
+    :show-inheritance:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- nem/importer.py
+++ nem/importer.py
@@ -10,26 +10,72 @@ from bs4 import BeautifulSoup
 
 # TODO DANGEROUS
 os.environ['TZ'] = 'Australia/Brisbane' #nem time is always in Sydney time
-print("This library has updated your timezone to Brisbane")
+print("This library has updated your timezone to Brisbane - I'm sorry")
 
 OLDEST_YEAR = 2009
 OLDEST_MONTH = 7
 class importer():
+    """Impoter class allows downloading data from AEMO NEM site
+
+    :param url:
+        URL to the NEM server
+    :type url: ``string``
+
+    :param historical:
+        Access to historical data using the MMSDM data
+    :type historical: ``boolean``
+
+    :returns: Returns an array of documents
+    :rtype: ``[document]``
+    """
     def __init__(self, url="http://www.nemweb.com.au/Reports/CURRENT/", historical=False):
         if historical==False:
             self.p5 = files(url + "P5_Reports/", historical)
+            """NEM p5 data documents
+            
+            :rtype: [document]
+            """
             self.DispatchIS = files(url + "DispatchIS_Reports/", historical)
+            """NEM DispatchIS data documents
+            
+            :rtype: [document]
+            """
             self.Notices = files(url + "Market_Notice/", historical)
+            """NEM Notices data documents
+            
+            :rtype: [document]
+            """
             self.SCADA = files(url + "Dispatch_SCADA/", historical)
-            #self.CO2 = files(url + "CDEII/", historical)
+            """NEM SCADA data documents
+            
+            :rtype: [document]
+            """
+            self.CO2 = files(url + "CDEII/", historical)
+            """NEM CO2 data documents
+            
+            :rtype: [document]
+            """
         else:
             self.p5 = files(url, "PUBLIC_DVD_P5MIN_REGIONSOLUTION")
             self.DispatchIS = files(url, "PUBLIC_DVD_DISPATCHREGIONSUM")
             # self.Notices = files(url + "Market_Notice/", historical) #TODO
             self.SCADA = files(url, "PUBLIC_DVD_DISPATCH_UNIT_SCADA")
-            #self.CO2 = files(url, "PUBLIC_DVD_BILLING_CO2E_PUBLICATION")
+            self.CO2 = files(url, "PUBLIC_DVD_BILLING_CO2E_PUBLICATION")
 
 class files(list):
+    """Lists all the documents avaliable for parsing by this module
+
+    :param baseUrl:
+        URL to list files for
+    :type baseUrl: ``string``
+
+    :param historical:
+        Enables generating a list of files for every year, requires being set to the filename requested (eg PUBLIC_DVD_P5MIN_REGIONSOLUTION)
+    :type historical: ``string``
+
+    :returns: Returns an array of documents
+    :rtype: ``[document]``
+    """
     def __init__(self, baseUrl, historical):
         if historical == False:
             indexPage = BeautifulSoup(urlopen(baseUrl).read(), "html.parser")
@@ -52,6 +98,12 @@ class files(list):
 
 
 class document():
+    """Lists all the documents avaliable for parsing by this module
+
+    :param url:
+        URL to parse
+    :type url: ``string``
+    """
     def __init__(self, url):
         self.url = url
         self._cached = None
@@ -66,6 +118,10 @@ class document():
                 except ValueError:
                     dateObject = datetime.strptime(strDate, "%Y%m%d%H%M%S")
             self.dateTime = dateObject
+            """Provides a datetime object for the file based on the timestamp in the filename
+
+            :rtype: datetime
+            """
         except(IndexError):
             pass
 
@@ -78,6 +134,11 @@ class document():
 
     @property
     def data(self):
+        """Provides the raw data from the file
+
+        :rtype: list
+        :returns: Array of raw lines of data
+        """
         if not self._cached:
             if self.url[-4:].lower() != ".csv":
                 self._cached = self._extract(self.url)
@@ -87,13 +148,16 @@ class document():
         return self._cached
 
     def _extract(self, url):
-        print("Downloading and unzipping " + url)
         file = ZipFile(BytesIO(urlopen(url).read()))
         data = file.read(file.namelist()[0])
-        print("Finished downloading " + url)
         return data
     @property
     def datasets(self):
+        """Provides lists of datasets in the file
+
+        :rtype: list
+        :returns: Array of datasets avaliable
+        """
         csvfile = csv.reader(self.data)
         data = []
         for row in csvfile:
@@ -105,6 +169,11 @@ class document():
         return data
     @property
     def clean_data(self):
+        """Provides a dictonary of all the datasets
+
+        :rtype: object
+        :returns: {"dataset1":[{"header1":"value"}]}
+        """
         csvfile = csv.reader(self.data)
         headers = []
         data = {}
@@ -135,6 +204,10 @@ class document():
             
         return x    
     def filter(self, dataSet):
+        """Filters out data to a specific dataset and return that set
+
+        :rtype: [{"header1":"data1"}]
+        """
         csvfile = csv.reader(self.data)
         headers = []
         data = []
